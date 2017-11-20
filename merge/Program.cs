60c35b6485c2134adb4299a1a93fa125278aa5d5
Edison <edison@cladian.com>
2017-11-20 10:57:43 -0500

    emerge C#

---
+++ merge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Validation;
+
+namespace merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(File.Exists("ppt/file1.pptx"));
+            Console.Read();
+
+            int id = 0;
+            using (PresentationDocument myDestDeck = PresentationDocument.Open("ppt/file1.pptx", true)) {
+                PresentationPart destPresPart = myDestDeck.PresentationPart;
+
+                if (destPresPart.Presentation.SlideIdList == null)
+                    destPresPart.Presentation.SlideIdList = new SlideIdList();
+            }
+        }
+    }
+}
